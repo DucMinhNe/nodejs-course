a6b858003f62ba3ecee0418f52c73c4acf2745a6
Lê Đức Minh <ducminhldm@gmail.com>
2023-08-05 23:40:29 +0700

    add type-coercion

---
+++ topics/type-coercion/README.md
@@ -0,0 +1,16 @@
+# Type Coercion & Equality
+
+JS coerces values when comparing or operating across types.
+
+## Key points
+
+- `==` performs type coercion, `===` does not — prefer `===`
+- falsy values: 0, '', null, undefined, NaN, false
+
+## Examples
+
+See `examples/` for runnable demos.
+
+## Exercise
+
+Predict the output of 5 `==` traps.
---
+++ topics/type-coercion/examples/01-coercion.js
@@ -0,0 +1,10 @@
+console.log(0 == '');       // true
+console.log(0 == '0');      // true
+console.log('' == '0');     // false
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+console.log(NaN === NaN);   // false (always)
+console.log([] == false);   // true
+console.log([] == 0);       // true
+console.log(1 + '2');       // '12'
+console.log('1' - 2);       // -1
